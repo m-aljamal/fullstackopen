b4e4c975d08f5b0c8f8449c3e34aa24fd0c52289
m-aljamal <m-aljamal@github.com>
2020-04-26 11:16:35 +0300

    unicafe step6

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -15,19 +15,24 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Give feedback</h1>
       <Buttons feedback={feedback} setfeedback={setfeedback} />
       <h1>Statistics</h1>
       {good === 0 && neutral === 0 && bad === 0 ? (
         <h2>No feedback given</h2>
       ) : (
-        <>
+        <table>
+          <tr>
+            <th>Statistic</th>
+            <th>Value</th>
+          </tr>
           <Statistic text="Good" value={good} />
           <Statistic text="Neutral" value={neutral} />
           <Statistic text="Bad" value={bad} />
           <Statistic text="All" value={total} />
-          <Statistic text="Average" value={average} />
-          <Statistic text="Positive" value={positive} />
-        </>
+          <Statistic text="Average" value={average.toFixed(3)} />
+          <Statistic text="Positive" value={`${positive.toFixed(3)} % `} />
+        </table>
       )}
     </div>
   );
