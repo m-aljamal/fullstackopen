bde7880d0148801e5a5e041e553b4a25bfa7ff0d
m-aljamal <m-aljamal@github.com>
2020-04-26 10:35:18 +0300

    unicafe step 5

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 import "./App.css";
 import Buttons from "./components/Buttons";
-import Statistics from "./components/Statistics";
-
+import Statistic from "./components/Statistic";
 function App() {
   const [feedback, setfeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good * 100) / total;
 
   return (
     <div className="App">
       <Buttons feedback={feedback} setfeedback={setfeedback} />
       <h1>Statistics</h1>
-      <Statistics feedback={feedback} />
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <h2>No feedback given</h2>
+      ) : (
+        <>
+          <Statistic text="Good" value={good} />
+          <Statistic text="Neutral" value={neutral} />
+          <Statistic text="Bad" value={bad} />
+          <Statistic text="All" value={total} />
+          <Statistic text="Average" value={average} />
+          <Statistic text="Positive" value={positive} />
+        </>
+      )}
     </div>
   );
 }
