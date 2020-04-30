25effded49b6b39c6f950ed176f175b05b25a263
m-aljamal <m-aljamal@github.com>
2020-05-01 01:52:43 +0300

    the phonebook step 4

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -5,14 +5,16 @@ import Person from "./components/Person";
 function App() {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "04-09-255588" },
+    { name: "Muhammed", number: "04-09-2777777" },
+    { name: "Yassen", number: "08-09-255588" },
   ]);
   const [formData, setFormData] = useState({ name: "", number: "" });
-  // const [newNumber, setNewNumber] = useState("");
+
   const { name, number } = formData;
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
+  const [serchName, setSerchName] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     const checkName = persons.find(
@@ -24,9 +26,17 @@ function App() {
     setFormData({ name: "", number: "" });
   };
 
+  const handleSerch = (e) => {
+    const serchName = persons.filter((per) =>
+      per.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    serchName && setSerchName(serchName);
+  };
+
   return (
     <div className="App">
       <h2>Phonebook</h2>
+      <input type="text" onChange={handleSerch} />
       <form onSubmit={handleSubmit}>
         <div>
           name:{" "}
@@ -46,8 +56,12 @@ function App() {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons &&
-        persons.map((person) => <Person key={person.name} person={person} />)}
+      {serchName.length > 0
+        ? serchName.map((person) => (
+            <Person key={person.name} person={person} />
+          ))
+        : persons &&
+          persons.map((person) => <Person key={person.name} person={person} />)}
     </div>
   );
 }
