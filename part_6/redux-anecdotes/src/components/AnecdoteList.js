e463f858c8261245a3ace53c5a7cae9f000a612c
m-aljamal <m-aljamal@github.com>
2020-06-05 21:00:13 +0300

    anecdotes and connect

--- part_6/redux-anecdotes/src/components/AnecdoteList.js
+++ part_6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,40 +1,59 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 import { addVote } from "../reducers/anecdoteReducer";
 import { addAlert } from "../reducers/notificationReducer";
+import { connect } from "react-redux";
+const AnecdoteList = (props) => {
+  // const filterText = useSelector(({ filterReducer }) => filterReducer);
 
-const AnecdoteList = () => {
-  const filterText = useSelector(({ filterReducer }) => filterReducer);
-
-  const anecdotes = useSelector(({ anecdoteReducer }) => {
-
-    return anecdoteReducer
-      .filter((ane) =>
-        ane.content.toLowerCase().includes(filterText.toLowerCase())
-      )
-      .sort((a, b) => b.votes - a.votes);
-  });
-  const dispatch = useDispatch();
+  // const anecdotes = useSelector(({ anecdoteReducer }) => {
+  //   return anecdoteReducer
+  //     .filter((ane) =>
+  //       ane.content.toLowerCase().includes(filterText.toLowerCase())
+  //     )
+  //     .sort((a, b) => b.votes - a.votes);
+  // });
+  // const dispatch = useDispatch();
   return (
     <div>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes} votes
-            <button
-              onClick={() => {
-                dispatch(addVote(anecdote.id));
-                dispatch(addAlert(`voting for ${anecdote.content}`, "normal"));
-              }}
-            >
-              vote
-            </button>
+      {props.anecdoteReducer
+        .filter((ane) =>
+          ane.content.toLowerCase().includes(props.filterReducer.toLowerCase())
+        )
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes} votes
+              <button
+                onClick={() => {
+                  // dispatch(addVote(anecdote.id));
+                  // dispatch(addAlert(`voting for ${anecdote.content}`, "normal"));
+                  props.addVote(anecdote.id);
+                  props.addAlert(`voting for ${anecdote.content}`, "normal");
+                }}
+              >
+                vote
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    anecdoteReducer: state.anecdoteReducer,
 
-export default AnecdoteList;
+    filterReducer: state.filterReducer,
+  };
+};
+
+const mapDispatchToProps = {
+  addVote,
+  addAlert,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnecdoteList);
